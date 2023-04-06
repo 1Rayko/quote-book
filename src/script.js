4535f867d9e959c11ed9919fd7d7ed38875ba69f
Radomir Sudoreboot <55040629+1Rayko@users.noreply.github.com>
2023-04-06 18:53:56 +0700

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,25 +1,29 @@
 
-const API_KEY = "M4kIaCbhZNehvv7g1m6wBDEFaw2HLQjesC985Nj2";
 
-// Функция для получения цитаты из API
-function getQuote() {
-  // URL-адрес API для получения случайной цитаты
-  const apiUrl = `https://api.theysaidso.com/qod.json?category=inspire&api_key=${API_KEY}`;
+$(document).ready(function() {
+  // Загрузка ключа API 
+ const API_KEY = "M4kIaCbhZNehvv7g1m6wBDEFaw2HLQjesC985Nj2";
 
-  // Отправка запроса на получение цитаты
-  $.getJSON(apiUrl, function(data) {
-    // Извлечение текста цитаты из ответа API
-    const quote = data.contents.quotes[0].quote;
-    
-    // Обновление текста цитаты на странице
-    $("#quote").text(quote);
-  });
-}
+  // Функция для получения цитаты из API
+  function getQuote() {
+    // URL-адрес API для получения случайной цитаты
+    const apiUrl = `https://api.theysaidso.com/qod.json?category=inspire&api_key=${API_KEY}`;
 
-// Обновление цитаты при загрузке страницы
-getQuote();
+    // Отправка запроса на получение цитаты
+    $.getJSON(apiUrl, function(data) {
+      // Извлечение текста цитаты из ответа API
+      const quote = data.contents.quotes[0].quote;
+      
+      // Обновление текста цитаты на странице
+      $("#quote").text(quote);
+    });
+  }
 
-// Обновление цитаты при клике на кнопку "New Quote"
-$("#new-quote").click(function() {
+  // Обновление цитаты при загрузке страницы
   getQuote();
+
+  // Обновление цитаты при клике на кнопку "New Quote"
+  $("#new-quote").click(function() {
+    getQuote();
+  });
 });
