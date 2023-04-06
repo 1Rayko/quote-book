73fd314419219249a61f2f5ab3a26905441aa9e9
Radomir Sudoreboot <55040629+1Rayko@users.noreply.github.com>
2023-04-06 18:50:13 +0700

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,11 +1,27 @@
+$(document).ready(function() {
+  // Загрузка ключа API из файла .env
+  const API_KEY = "M4kIaCbhZNehvv7g1m6wBDEFaw2HLQjesC985Nj2";
 
-function generateQuote() {
-  fetch('https://quotes.rest/quote/random.json')
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById('quote').innerHTML = `"${data.contents.quote}" - ${data.contents.author}`;
-    })
-    .catch(error => {
-      console.log(error);
+  // Функция для получения цитаты из API
+  function getQuote() {
+    // URL-адрес API для получения случайной цитаты
+    const apiUrl = `https://api.theysaidso.com/qod.json?category=inspire&api_key=${API_KEY}`;
+
+    // Отправка запроса на получение цитаты
+    $.getJSON(apiUrl, function(data) {
+      // Извлечение текста цитаты из ответа API
+      const quote = data.contents.quotes[0].quote;
+      
+      // Обновление текста цитаты на странице
+      $("#quote").text(quote);
     });
-}
+  }
+
+  // Обновление цитаты при загрузке страницы
+  getQuote();
+
+  // Обновление цитаты при клике на кнопку "New Quote"
+  $("#new-quote").click(function() {
+    getQuote();
+  });
+});
